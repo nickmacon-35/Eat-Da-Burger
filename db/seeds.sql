98887eed51f6a91c8b61d5b335160e24725568b5
Nick Macon <nickmacon@gmail.com>
2020-01-18 12:59:13 -0600

    1.18 13:00 push

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('BBQ bacon cheddar burger');
+INSERT INTO burgers (name) VALUES ('Mushroom swiss burger');
+INSERT INTO burgers (name, devoured) VALUES ('Chili cheese burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Whataburger', true);
+INSERT INTO burgers (name) VALUES ('Veggie burger');
